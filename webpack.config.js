7fa9bfddc0e637942d53514b160346f23a2891d8
Maxim Nikonov <the_roffler@mail.ru>
2022-10-15 19:17:23 +0300

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 const path = require('path');
+// eslint-disable-next-line import/no-extraneous-dependencies
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV === 'production';
@@ -25,8 +24,19 @@ const config = {
   module: {
     rules: [
       {
+        test: /\.(js|jsx)$/i,
+        loader: 'babel-loader',
+      },
+      {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: 'asset',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
       },
 
       // Add your rules for custom modules here
