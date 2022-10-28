26251309d9f5a34a3246bf2de9a5b326c8c6f6a2
Maxim Nikonov <the_roffler@mail.ru>
2022-10-28 16:40:05 +0300

    fix some files

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import i18next from 'i18next';
 import { setLocale } from 'yup';
 import view from './view.js';
 import resources from './locales/index.js';
-import parse from './parser.js';
+import parseRSS from './RSS_parser.js';
 
 const app = () => {
   const defaultLanguage = 'ru';
@@ -68,7 +68,7 @@ const app = () => {
   };
 
   const loadRSS = (link) => axios.get(allOrigin(link))
-    .then((response) => parse(link, response.data.contents))
+    .then((response) => parseRSS(link, response.data.contents))
     .then((parsedData) => addPostId(parsedData));
 
   const updateRSS = (link) => {
