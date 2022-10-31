924ecd17a50420b55146e76c87324d995f4b0bcd
Maxim Nikonov <the_roffler@mail.ru>
2022-10-31 18:07:38 +0300

    fix files

--- src/parser.js
+++ src/parser.js
@@ -1,36 +1,36 @@
-const parseToHTML = (data) => {
-  const parser = new DOMParser();
-  const parsedData = parser.parseFromString(data, 'application/xml');
-  const errorNode = parsedData.querySelector('parsererror');
-  if (errorNode) throw new Error('rssError');
-  return parsedData;
-};
-
-const getFeed = (parsedData) => {
-  const titleEl = parsedData.querySelector('channel title');
-  const title = titleEl.textContent;
-  const descriptionEl = parsedData.querySelector('channel description');
-  const description = descriptionEl.textContent;
-  return { title, description };
-};
-
-const getPosts = (parsedData) => {
-  const items = parsedData.querySelectorAll('item');
-  return [...items].map((item) => {
-    const titleEl = item.querySelector('title');
-    const title = titleEl.textContent;
-    const descriptionEl = item.querySelector('description');
-    const description = descriptionEl.textContent;
-    const linkEl = item.querySelector('link');
-    const link = linkEl.textContent.trim();
-    return { title, description, link };
-  });
-};
-
 export default (data) => {
-  const parsedData = parseToHTML(data);
-  return {
-    feed: getFeed(parsedData),
-    posts: getPosts(parsedData),
+  const parser = new DOMParser();
+  const parsedData = parser.parseFromString(data, 'text/xml');
+  const parseError = parsedData.querySelector('parsererror');
+  if (parseError) {
+    throw new Error(parseError.textContent);
+  }
+  const result = {
+    feed: null,
+    posts: [],
   };
+
+  const feedTitle = parsedData.querySelector('title').textContent;
+  const feedDescription = parsedData.querySelector('description').textContent;
+
+  const posts = parsedData.querySelectorAll('item');
+  posts.forEach((post) => {
+    const postTitle = post.querySelector('title').textContent;
+    const postDescription = post.querySelector('description').textContent;
+    const postLink = post.querySelector('link').textContent;
+
+    const postData = {
+      title: postTitle,
+      description: postDescription,
+      link: postLink,
+    };
+    result.posts.push(postData);
+  });
+
+  result.feed = {
+    title: feedTitle,
+    description: feedDescription,
+  };
+
+  return result;
 };
