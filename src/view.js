fd6fd81cee872da63a122e1394046389ab5fbed5
Maxim Nikonov <the_roffler@mail.ru>
2022-10-31 17:26:03 +0300

    fix files

--- src/view.js
+++ src/view.js
@@ -156,7 +156,7 @@ const renderFeedback = (state, status, i18nextInstance) => {
       clearFeedback();
       input.classList.add('is-invalid');
       feedback.classList.add('text-danger');
-      feedback.textContent = state.form.error;
+      feedback.textContent = i18nextInstance.t('errors');
       break;
     case 'clear':
       clearFeedback();
